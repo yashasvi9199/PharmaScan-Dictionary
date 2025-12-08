f710a0468711ef3534cfb7197b0d6e877ddef51c
Yash Haldiya <yashasvi9199@gmail.com>
2025-12-08 16:59:39 +0530

    style: changed output messages

--- ingest/run_ingest.sh
+++ ingest/run_ingest.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env sh
 set -e
+echo " === Ingest sources ==="
+
+echo "  - openfda"
 node ingest/source_openfda.js
+
+echo "  - who_atc"
 node ingest/source_who_atc.js
+
+echo " === Merge ingested ==="
 node ingest/merge_ingested.js
+
 echo "OK"
